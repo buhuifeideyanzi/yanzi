f102e663949540c14caf3ef73b43a553af508cde
buhuifeideyanzi <74659905+buhuifeideyanzi@users.noreply.github.com>
2020-11-30 12:01:46 +0800

    继承父类R1

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,20 @@
+package java1130;
+
+public class R1 {
+	static void TuPian() {
+		String boy_filePath="img/boy.gif";
+		String girl_filePath="img/girl.gif";
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		int[][]array2d= {{1,2,3,},{4,5,6}};
+		for (int row = 0; row< array2d.length; row++) {
+			for (int colum = 0; colum < array2d[0].length; colum++) {
+				System.out.print(array2d[row][colum]+" ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
